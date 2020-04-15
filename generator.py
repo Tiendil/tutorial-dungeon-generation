8edbfbb279e5b69e8d7bc38718f3d5ad3ebd40aa
Tiendil <a.eletsky@gmail.com>
2020-04-15 18:02:32 +0300

    step 6: beautify dungeons

--- generator.py
+++ generator.py
@@ -11,9 +11,10 @@ parser = argparse.ArgumentParser(description='Generate dungeon.')
 
 parser.add_argument('-f', '--filename', metavar='FILENAME', type=str, default='last.png', help='save result to file')
 parser.add_argument('-s', '--show', action='store_true', default=False, help='show result in window')
-parser.add_argument('-b', '--blocks', type=int, default=10, help='blocks in room')
-parser.add_argument('-r', '--rooms', type=int, default=5, help='rooms in dungeon')
-parser.add_argument('-d', '--doors', type=int, default=3, help='doors in room')
+parser.add_argument('-b', '--blocks', type=int, default=(3, 15), nargs=2, help='blocks in room [min, max]')
+parser.add_argument('-r', '--rooms', type=int, default=25, help='rooms in dungeon')
+parser.add_argument('-d', '--doors', type=int, default=(2, 4), nargs=2, help='doors in room [min, max]')
+parser.add_argument('--show-doors', action='store_true', default=False, help='show doors')
 
 arguments = parser.parse_args()
 
@@ -49,23 +50,39 @@ def points_at_circle(x, y, radius):
     return points
 
 
-def path_length(point_from, point_to, filled_cells, max_path_length):
+def restore_path(path_map, point):
+    path = []
+
+    while point is not None:
+        path.append(point)
+        point = path_map[point]
+
+    path.reverse()
+
+    return path
+
+
+def find_path(point_from, point_to, filled_cells, max_path_length):
 
     index = 0
 
-    heap = [(0, index, point_from)]
+    heap = [(0, index, point_from, None)]
 
     visited_points = {}
 
+    path_map = {}
+
     while True:
 
-        cost, _, point = heapq.heappop(heap)
+        cost, _, point, prev_point = heapq.heappop(heap)
+
+        path_map[point] = prev_point
 
         if max_path_length <= cost:
-            return None
+            return None, None
 
         if point == point_to:
-            return cost
+            return cost, restore_path(path_map, point_to)
 
         visited_points[point] = cost
 
@@ -77,9 +94,30 @@ def path_length(point_from, point_to, filled_cells, max_path_length):
                 continue
 
             index += 1
-            heapq.heappush(heap, (cost + 1, index, next_point))
+            heapq.heappush(heap, (cost + 1, index, next_point, point))
 
-    return None
+    return None, None
+
+
+def make_countur(segments):
+
+    segments = list(segments)
+
+    line = list(segments.pop())
+
+    while True:
+
+        end_point = line[-1]
+
+        for segment in segments:
+            if end_point == segment[0]:
+                line.append(segment[1])
+                segments.remove(segment)
+                break
+        else:
+            break
+
+    return line
 
 
 ##############
@@ -357,15 +395,21 @@ class Room:
 
 
 class Corridor:
-    __slots__ = ('start_border', 'stop_border')
+    __slots__ = ('start_border', 'stop_border', 'path')
 
-    def __init__(self, start_border, stop_border):
+    def __init__(self, start_border, stop_border, path):
         self.start_border = start_border
         self.stop_border = stop_border
+        self.path = path
 
     def geometry_segments(self):
-        return [self.start_border.connection_point(),
-                self.stop_border.connection_point()]
+        points = [self.start_border.connection_point()]
+
+        points.extend(position.move(0.5, 0.5).point() for position in self.path)
+
+        points.append(self.stop_border.connection_point())
+
+        return points
 
 
 class Dungeon:
@@ -378,10 +422,10 @@ class Dungeon:
     def create_room(self, blocks, doors):
         room = Room()
 
-        for i in range(blocks):
+        for i in range(random.randint(*blocks)):
             room.expand()
 
-        room.place_doors(doors)
+        room.place_doors(random.randint(*doors))
 
         return room
 
@@ -441,6 +485,8 @@ class Dungeon:
 
         dungeon_positions = self.block_positions()
 
+        corridor_path = None
+
         # ATTENTION: method room_positions_bruteforce make modifications of new_room
         #            it is not very good decission
         for max_distance, dungeon_door, new_room_door, x, y in self.room_positions_bruteforce(max_intersection_radius,
@@ -451,12 +497,12 @@ class Dungeon:
 
             filled_positions = dungeon_positions | new_room.block_positions()
 
-            real_path_length = path_length(dungeon_door_out_position,
-                                           new_room_door_out_position,
-                                           filled_cells=filled_positions,
-                                           max_path_length=max_distance)
+            path_length, corridor_path = find_path(dungeon_door_out_position,
+                                                   new_room_door_out_position,
+                                                   filled_cells=filled_positions,
+                                                   max_path_length=max_distance)
 
-            if real_path_length is None:
+            if path_length is None:
                 continue
 
             break
@@ -468,7 +514,7 @@ class Dungeon:
 
         # ATTENTION: it is very bad decission, to store objects by links in two different parent objects
         #            beteer solution will be to store threre ID's or something similar
-        new_corridor = Corridor(dungeon_door, new_room_door)
+        new_corridor = Corridor(dungeon_door, new_room_door, corridor_path)
 
         self.corridors.append(new_corridor)
 
@@ -481,6 +527,7 @@ class Dungeon:
 dungeon = Dungeon()
 
 for i in range(arguments.rooms):
+    print('generate room', i + 1)
     dungeon.expand(blocks=arguments.blocks,
                    doors=arguments.doors)
 
@@ -494,16 +541,21 @@ pyplot.axes().set_aspect('equal', 'datalim')
 fig = pyplot.figure(1)
 
 for room in dungeon.rooms:
-    for border in room.geometry_borders():
+    borders = list(room.geometry_borders())
+
+    pyplot.fill(*zip(*make_countur(borders)), '#ffffff')
+    pyplot.fill(*zip(*make_countur(borders)), room.color, alpha=0.5)
+
+    for border in borders:
         pyplot.plot(*zip(*border), color=room.color, linewidth=3, alpha=1.0)
 
-for room in dungeon.rooms:
-    for door_border in room.door_borders():
-        pyplot.plot(*zip(*door_border.geometry_borders()), color=room.color, linewidth=6, alpha=0.5)
-
+if arguments.show_doors:
+    for room in dungeon.rooms:
+        for door_border in room.door_borders():
+            pyplot.plot(*zip(*door_border.geometry_borders()), color=room.color, linewidth=6, alpha=0.5)
 
 for corridor in dungeon.corridors:
-    pyplot.plot(*zip(*corridor.geometry_segments()), color='#000000', linewidth=1, alpha=1)
+    pyplot.plot(*zip(*corridor.geometry_segments()), color='#000000', linewidth=3, alpha=1, zorder=0)
 
 if arguments.filename:
     pyplot.savefig(arguments.filename)
